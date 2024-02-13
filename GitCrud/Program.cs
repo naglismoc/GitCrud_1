4b5ce4c7f9b876c9c65cb0d2db67aee9c8b9295b
naglismoc <dvd.filmas@gmail.com>
2024-02-13 14:05:56 +0200

    Add project files.

---
+++ GitCrud/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GitCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            Console.WriteLine("=X");
+
+            Console.WriteLine(":O?");
+            int i = 0;
+            //HM.....
+        }
+    }
+}
